a3a94295d3977016952ecaef2155e8368f809ebc
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2022-12-19 14:38:57 +0100

    Exo14.c

--- exo14.c
+++ exo14.c
@@ -3,12 +3,12 @@
     void main () {
 
 for (int j=0;j<10;j++){
-   printf("\n");
+   
     for ( int i=0; i<10 ; ++i){
 
 
 
-    printf("%d * %d= %d",j,i,j*i);
+    printf(" %d * %d= %d \n",j,i,j*i);
 
 
 
