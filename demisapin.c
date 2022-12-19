0f2e540cf65cdd68b5af7a10820c880b2065e4f3
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2022-12-19 22:19:43 +0100

    Update demisapin.c

--- demisapin.c
+++ demisapin.c
@@ -1,78 +1,125 @@
 #include <stdio.h>
+#include <conio.h>
+#include <unistd.h>
 
 
 void sapin (int n) {
 
 
-    scanf("%d",&n);
 
-    for(int i=1;i<=n;i++)
+
+    for(int i=0;i<n;i++)
 {
-    printf("  ");
-    printf("  ");
-    printf("  ");
-    printf("  ");
-    for(int j=1;j<=i;j++){
 
-        printf("*");
+        for(int j=(n-i)-1;j>0;j--){
+
+        printf(" ");
 
     }
+            for(int c=(2*i)+1;c>0;c--)
+            {
+                printf("*");
+            }
 
 printf("\n");
 
 
 }
 
-for(int l=1;l<=n;l++)
+for(int v=1;v<n;v++)
 {
-     printf("  ");
-     printf("  ");
-     printf("  ");
-     printf("  ");
-    for(int k=1;k<=l;k++){
 
-        printf("*");
+        for(int k=(n-v)-1;k>0;k--){
+
+        printf(" ");
 
     }
+            for(int z=(2*v)+1;z>0;z--)
+            {
+                printf("*");
+            }
+
 printf("\n");
-
-
-
-
 
 
 
 }
 
+for(int v=1;v<2;v++)
+{
 
-printf("  ");
-printf("  ");
-printf("  ");
-printf("  ");
+        for(int k=2*(n-v)-n+v;k<=2*(n-v)-10/8;k++){
 
-printf("@\n");
-
-printf("  ");
-printf("  ");
-printf("  ");
-printf("  ");
-
-printf("@");
-
+        printf(" ");
 
     }
+            for(int z=1;z<=1;z++)
+            {
+                printf("|");
+            }
 
-
+printf("\n");
+}
+}
 void main(){
 
 int n;
 
-    printf("Entrez la hauteur souhaiter:");
+    printf("Entrez la hauteur souhaiter:\n");
+    scanf("%d",&n);
+    system("COLOR 0A");
     sapin(n);
+    sleep(1);
+    system("cls");
+    system("COLOR 0C");
+    printf("Entrez la hauteur souhaiter:\n");
+    scanf("%d",&n);
+    sapin(n);
+    sleep(1);
+    system("cls");
+    system("COLOR 0B");
+    printf("Entrez la hauteur souhaiter:\n");
+    scanf("%d",&n);
+    sapin(n);
+    sleep(1);
+    system("cls");
+    system("COLOR 0D");
+    printf("Entrez la hauteur souhaiter:\n");
+    scanf("%d",&n);
+    sapin(n);
+    sleep(1);
+    system("cls");
+    system("COLOR 0E");
+    printf("Entrez la hauteur souhaiter:\n");
+    scanf("%d",&n);
+    sapin(n);
+    sleep(1);
+    system("cls");
+    system("COLOR 0C");
+    printf("Entrez la hauteur souhaiter:\n");
+    scanf("%d",&n);
+    sapin(n);
+    sleep(1);
+    system("cls");
 
+     n=7;
 
-
-
-
+    system("COLOR 0A");
+    sapin(n);
+    sleep(1);
+    system("COLOR 0C");
+    sleep(1);
+    system("COLOR 0B");
+    sleep(1);
+    printf("\n");
+    system("COLOR 0D");
+    sleep(1);
+    system("COLOR 0E");
+    sleep(1);
+    system("COLOR 0C");
+    sleep(1);
+    system("COLOR 0B");
+    sleep(1);
+    system("COLOR 0A");
 
 }
